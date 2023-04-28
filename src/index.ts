eb193bdccad2229bbb99a5411892cbdc1221866d
AiharaMahiru <vni@outlook.jp>
2023-04-28 10:43:50 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,23 @@
+import './style/index.less';
+console.log('hello world');
 
-let box1 = document.getElementById('box1');
-box1?.addEventListener('click', function () {
-    alert("hello");
-});
+// class Food{
+//     element: HTMLElement;
+//     constructor(){
+//         this.element = document.getElementById('food')!;
+//     }
+//     get X(){
+//         return this.element.offsetLeft;
+//     }
+//     get Y(){
+//         return this.element.offsetTop;
+//     }
+//     change(){
+//         let left = Math.round(Math.random() * 29) * 10;
+//         let top = Math.round(Math.random() * 29) * 10;
+//         this.element.style.left = left + 'px';
+//         this.element.style.top = top + 'px';
+//     }
+// }
+// const food = new Food();
+// console.log(food.X, food.Y);
--- webpack.config.js
+++ webpack.config.js
@@ -8,34 +8,61 @@ module.exports = {
     entry: './src/index.ts',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
+        filename: 'bundle.js',
+        environment:{
+            arrowFunction: false,
+            const: false
+        }
     },
     module:{
         rules:[
             {
                 test: /\.ts$/,
                 use: [
-                    // {
-                    //     loader: 'babel-loader',
-                    //     options: {
-                    //         presets: [
-                    //             [
-                    //                 '@babel/preset-env',
-                    //                 {
-                    //                     targets: {
-                    //                         chrome: '102',
-                    //                         ie: '11'
-                    //                     },
-                    //                     corejs: '3',
-                    //                     useBuiltIns: 'usage'
-                    //                 }
-                    //             ]
-                    //         ]
-                    //     }
-                    // },
-                    'ts-loader', 
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: [
+                                [
+                                    '@babel/preset-env',
+                                    {
+                                        targets: {
+                                            chrome: '102',
+                                            ie: '11'
+                                        },
+                                        corejs: '3',
+                                        useBuiltIns: 'usage'
+                                    }
+                                ]
+                            ]
+                        }
+                    },
+                    'ts-loader' 
                 ],
                 exclude: /node_modules/
+            },
+            {
+                test: /\.less$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    {
+                        loader:'postcss-loader',
+                        options:{
+                            postcssOptions:{
+                                plugins:[
+                                    [
+                                        'postcss-preset-env',
+                                        {
+                                            browsers: 'last 2 versions'
+                                        }
+                                    ]
+                                ]
+                            }
+                        }
+                    },
+                    'less-loader',
+                ]
             }
         ]
     },
@@ -49,5 +76,5 @@ module.exports = {
     ],
     resolve: {
         extensions: ['.ts', '.js']
-    },
+    }
 }
