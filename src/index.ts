d956cb072a0334f313acaa3190dedb32f200a09d
AiharaMahiru <vni@outlook.jp>
2023-05-03 22:50:34 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,36 @@
 import './style/index.less';
-console.log('hello world');
+// console.log('hello world');
 
-// class Food{
-//     element: HTMLElement;
-//     constructor(){
-//         this.element = document.getElementById('food')!;
-//     }
-//     get X(){
-//         return this.element.offsetLeft;
-//     }
-//     get Y(){
-//         return this.element.offsetTop;
-//     }
-//     change(){
-//         let left = Math.round(Math.random() * 29) * 10;
-//         let top = Math.round(Math.random() * 29) * 10;
-//         this.element.style.left = left + 'px';
-//         this.element.style.top = top + 'px';
-//     }
-// }
-// const food = new Food();
-// console.log(food.X, food.Y);
+class Food{
+    element: HTMLElement;
+    constructor(){
+        this.element = document.getElementById('food')!;
+    }
+    get X(){
+        return this.element.offsetLeft;
+    }
+    get Y(){
+        return this.element.offsetTop;
+    }
+    change(){
+        let left = Math.round(Math.random() * 29) * 10;
+        let top = Math.round(Math.random() * 29) * 10;
+        this.element.style.left = left + 'px';
+        this.element.style.top = top + 'px';
+    }
+}
+
+class ScorePanel{
+    score = 0;
+    level = 1;
+    scoreEle: HTMLElement;
+    levelEle: HTMLElement;
+    maxLevel: number;
+    upScore: number;
+    constructor(maxLevel: number = 10, upScore: number = 10){
+        this.scoreEle = document.getElementById('score')!;
+        this.levelEle = document.getElementById('level')!;
+        this.maxLevel = maxLevel;
+        this.upScore = upScore;
+    }
+}
