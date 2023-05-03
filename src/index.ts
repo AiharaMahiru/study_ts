13b708ea67e26d18e19f0cdb3617c76bf12e0570
AiharaMahiru <vni@outlook.jp>
2023-05-03 23:05:31 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -27,10 +27,26 @@ class ScorePanel{
     levelEle: HTMLElement;
     maxLevel: number;
     upScore: number;
-    constructor(maxLevel: number = 10, upScore: number = 10){
+    constructor(maxLevel: number = 10, upScore: number = 99){
         this.scoreEle = document.getElementById('score')!;
         this.levelEle = document.getElementById('level')!;
         this.maxLevel = maxLevel;
         this.upScore = upScore;
     }
+    addScore(){
+        this.scoreEle.innerHTML = ++this.score + '';
+        if (this.score % this.upScore === 0){
+            this.levelUp();
+        }
+    }
+    levelUp(){
+        if(this.level < this.maxLevel && this.score % this.upScore === 0){
+            this.levelEle.innerHTML = ++this.level + '';
+        }
+    }
+}
+const scorePanel = new ScorePanel();
+// scorePanel.addScore();
+for(let i = 0; i < 100; i++){
+    scorePanel.addScore();
 }
